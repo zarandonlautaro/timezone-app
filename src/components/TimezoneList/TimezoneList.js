387215c1261e58e0f7f8df376e217834b6571f12
lzarandon <lautaro.zarandon@coderio.co>
2022-02-03 18:39:08 -0300

    timezone list update

--- src/components/TimezoneList/TimezoneList.js
+++ src/components/TimezoneList/TimezoneList.js
@@ -1,32 +1,31 @@
 import React from "react";
 import "./TimezoneList-module.css";
 
-const TimezoneList = (props) => {
-  const { timezoneList = [], handleRemove } = props;
-  return (
-    <>
-      {timezoneList.map((data) => {
-        if (!data) {
-          return null;
-        }
-        const { _id: id, timezone, unixtime } = data;
-        const milliseconds = unixtime * 1000;
-        const dateObject = new Date(milliseconds);
-        const humanDateFormat = dateObject.toLocaleString();
-        return (
-          <div key={id} className='box'>
-            <div className='box-container'>
-              <h2 className='title'>{timezone}</h2>
-              <p className='text'>{humanDateFormat}</p>
-            </div>
-            <span type='button' onClick={() => handleRemove(id)}>
-              ✖️
-            </span>
-          </div>
-        );
-      })}
-    </>
-  );
+const TimezoneList = ({ timezoneList = [], handleRemove }) => {
+    return (
+        <>
+            {timezoneList.map((timeZ, index) => {
+                if (!timeZ) {
+                    return null;
+                }
+                const { _id: id, timezone, unixtime } = timeZ;
+                const milliseconds = unixtime * 1000;
+                const dateObject = new Date(milliseconds);
+                const humanDateFormat = dateObject.toLocaleString();
+                return (
+                    <div key={index} className="box">
+                        <div className="box-container">
+                            <h4 className="title">{timezone}</h4>
+                            <p className="text">{humanDateFormat}</p>
+                        </div>
+                        <span type="button" onClick={() => handleRemove(id)}>
+                            ✖️
+                        </span>
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 
 export default TimezoneList;
